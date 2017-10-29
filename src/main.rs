22871e284125c7b1069fb7a7a07d3b5ffcd26054
John Barker <jebarker@gmail.com>
2017-10-29 12:44:10 -0400

    Add in basic index.html and redirect

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,19 @@
 
 extern crate rocket;
 
+use std::path::{Path};
+use rocket::response::{NamedFile, Redirect};
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn redirect_to_root() -> Redirect {
+    Redirect::to("/index.html")
+}
+
+#[get("/index.html")]
+fn serve_static_index() -> Option<NamedFile> {
+    NamedFile::open(Path::new("site/index.html")).ok()
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![redirect_to_root, serve_static_index]).launch();
 }
