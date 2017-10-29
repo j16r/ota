ad8e94679cdfc9f977129e5ce581fb47451dfd00
John Barker <jebarker@gmail.com>
2017-10-29 17:21:18 -0400

    Render base index as handlebars template

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
-#![cfg_attr(feature="clippy", feature(plugin))]
 
+extern crate rocket_contrib;
 extern crate rocket;
+#[macro_use] extern crate serde_derive;
 
 use std::path::{Path, PathBuf};
 use rocket::response::{NamedFile, Redirect};
+use rocket_contrib::Template;
 
 #[get("/")]
 fn redirect_to_root() -> Redirect {
     Redirect::to("/index.html")
+}
+
+#[derive(Serialize)]
+struct IndexContext {
+    debug: bool,
+}
+
+#[get("/index.html")]
+fn serve_index() -> Template {
+    let context = IndexContext{debug: true};
+    Template::render("index", &context)
 }
 
 #[get("/<file..>")]
@@ -18,7 +31,11 @@ fn serve_static_assets(file: PathBuf) -> Option<NamedFile> {
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![
+    rocket::ignite()
+        .mount("/", routes![
                            redirect_to_root,
-                           serve_static_assets]).launch();
+                           serve_index,
+                           serve_static_assets])
+        .attach(Template::fairing())
+        .launch();
 }
