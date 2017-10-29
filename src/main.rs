2cdb960727540e2dab31054edfc7121a81dd2d3e
John Barker <jebarker@gmail.com>
2017-10-29 12:53:21 -0400

    Serve all static assets under site/

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
+#![cfg_attr(feature="clippy", feature(plugin))]
 
 extern crate rocket;
 
-use std::path::{Path};
+use std::path::{Path, PathBuf};
 use rocket::response::{NamedFile, Redirect};
 
 #[get("/")]
@@ -11,11 +12,13 @@ fn redirect_to_root() -> Redirect {
     Redirect::to("/index.html")
 }
 
-#[get("/index.html")]
-fn serve_static_index() -> Option<NamedFile> {
-    NamedFile::open(Path::new("site/index.html")).ok()
+#[get("/<file..>")]
+fn serve_static_assets(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("site/").join(file)).ok()
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![redirect_to_root, serve_static_index]).launch();
+    rocket::ignite().mount("/", routes![
+                           redirect_to_root,
+                           serve_static_assets]).launch();
 }
