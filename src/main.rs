3cbccb2f80d15c3a447ce94f0d957af1f686f38d
John Barker <jebarker@gmail.com>
2018-03-03 17:03:06 -0500

    Move CDN URL into constant / template

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use std::path::{Path, PathBuf};
 use rocket::response::{NamedFile, Redirect};
 use rocket_contrib::Template;
 
+const CLOUDFLARE_CDN_URL: &'static str = "https://cdnjs.cloudflare.com/ajax/libs/";
+
 #[get("/")]
 fn redirect_to_root() -> Redirect {
     Redirect::to("/index.html")
@@ -18,11 +20,16 @@ fn redirect_to_root() -> Redirect {
 struct IndexContext {
     debug: bool,
     nocdn: bool,
+    cdn_url: &'static str,
 }
 
 #[get("/index.html")]
 fn serve_index() -> Template {
-    let context = IndexContext{debug: false, nocdn: false};
+    let context = IndexContext{
+        debug: true,
+        nocdn: true,
+        cdn_url: CLOUDFLARE_CDN_URL,
+    };
     Template::render("index", &context)
 }
 
